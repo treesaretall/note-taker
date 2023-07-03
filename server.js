8e1a39020b4a1e97c7904c0f9a1a18ccfd5d972f
treesaretall <himatthewhurst@hotmail.com>
2023-07-03 13:35:54 -0400

    added port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/routes.js');
 
-const PORT = 5500;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
