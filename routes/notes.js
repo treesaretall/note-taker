7619636084a558037fd17b0cec04bc45f34b8cbb
treesaretall <himatthewhurst@hotmail.com>
2023-07-05 15:30:53 -0400

    figuring out delete bug

--- routes/notes.js
+++ routes/notes.js
@@ -12,12 +12,10 @@ notes.get('/', (req, res) => {
 
 notes.get('/:id', (req, res) => {
   console.info(`${req.method} request received for note ${req.params.id}`);
-  let found = false;
   const filteredNotes = [];
 
   noteDB.forEach(note => {
     if (note.note_id == req.params.id) {
-      found = true;
       filteredNotes.push(note);
       res.json(filteredNotes)
     }
@@ -43,34 +41,47 @@ notes.post('/', (req, res) => {
   }
 });
 
-notes.delete('/:id', (req, res) => {
-  const found = noteDB.some(idFilter(req));
-  const newNoteDB = noteDB.filter(note => !idFilter(req)(note))
-  console.info(`${req.method} request received to delete note ${req.params.id}`);
+// notes.delete('/:id', (req, res) => {
+//   const found = noteDB.some(idFilter(req));
+//   const newNoteDB = noteDB.filter(note => !idFilter(req)(note))
+//   console.info(`${req.method} request received to delete note ${req.params.id}`);
 
-  if (found) {
-    clearJSONFile('./db/db.json')
-    writeToFile('./db/db.json', newNoteDB)
-    res.json({
-      msg: 'Note deleted',
-      noteDB: newNoteDB
-    });
-  } else {
-    res.status(400).json({ msg: `No note with the id of ${req.params.id}` });
-  }
-});
+//   if (found) {
+//     clearJSONFile('./db/db.json')
+//     writeToFile('./db/db.json', newNoteDB)
+//     res.json({
+//       msg: 'Note deleted',
+//       noteDB: newNoteDB
+//     });
+//   } else {
+//     res.status(400).json({ msg: `No note with the id of ${req.params.id}` });
+//   }
+// });
 
 
 // notes.delete('/:id', (req, res) => {
-//  const { id } = req.params;
+//  const id = req.params.id;
 
 //  const notesIndex = noteDB.findIndex(p => p.note_id == id);
-
-//   noteDB.splice(notesIndex, 1);
-//   console.log(notesIndex)
-//  return res.send("Note deleted successfully");
+//   if (notesIndex > -1) {
+//     noteDB.splice(notesIndex, 1);
+//     console.log(notesIndex)
+//     return res.send("Note deleted successfully");
+//   } else {
+//     res.status(400).json({ msg: `No note with the id of ${id}` });
+//   };
 // });
 
+notes.delete('/:id', (req, res) => {
+  console.info(`${req.method} request received for note ${req.params.id}`);
+  noteDB.forEach((note, index) => {
+    if (note.note_id == req.params.id) {
+      noteDB.splice(index, 1);
+      res.json(`Note deleted successfully`);
+      console.log(note.note_id == req.params.id);
+    }
+  });
+});
 
 
 
